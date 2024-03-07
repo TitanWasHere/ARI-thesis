c6133e812722d68a08afa48cfe60e7ea797a4bd8
TitanWasHere <andrea.gravili8@gmail.com>
2024-03-07 16:39:22 +0100

    Finished ROS node for ONLY detecting pose and orientation of the ArUco markers

--- aruco_ws/src/detection/src/aruco_detect.py
+++ aruco_ws/src/detection/src/aruco_detect.py
@@ -17,66 +17,102 @@ from time import sleep
 
 ARUCOBASE_MARKER_ID = 1
 
-# Camera calibration values (RealSense D435)
-cx = 421.9996032714844
-cy = 237.9039764404297
-fx = 616.7684936523438
-fy = 615.880615234375
-k1 = 0.0
-k2 = 0.0
-k3 = 0.0
-p1 = 0.0
-p2 = 0.0
+# Camera calibration values (RealSense D435) - 848x480
+cx_RealSense = 421.9996032714844
+cy_RealSense = 237.9039764404297
+fx_RealSense = 616.7684936523438
+fy_RealSense = 615.880615234375
+k1_RealSense = 0.0
+k2_RealSense = 0.0
+k3_RealSense = 0.0
+p1_RealSense = 0.0
+p2_RealSense = 0.0
 
-INTRINSIC_CAMERA = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
-DISTORTION_CAMERA = np.array([k1, k2, p1, p2, k3])
+INTRINSIC_CAMERA_REALSENSE = np.array([[fx_RealSense, 0, cx_RealSense], [0, fy_RealSense, cy_RealSense], [0, 0, 1]])
+DISTORTION_CAMERA_REALSENSE = np.array([k1_RealSense, k2_RealSense, p1_RealSense, p2_RealSense, k3_RealSense])
 
+# Camera calibration values (RealSense D435) - 1280x960
+cx_RealSense_1280 = 642.2582577578172
+cy_RealSense_1280 = 474.1471906434584
+fx_RealSense_1280 = 999.461170663331
+fy_RealSense_1280 = 996.9611451866272
+# k1_RealSense_1280 = 0.164427473610091
+# k2_RealSense_1280 = -0.2717244716038656
+# k3_RealSense_1280 = 0.0
+# p1_RealSense_1280 = -0.002867946281892625
+# p2_RealSense_1280 = -9.69782173585606e-05
+k1_RealSense_1280 = 0.0
+k2_RealSense_1280 = 0.0
+k3_RealSense_1280 = 0.0
+p1_RealSense_1280 = 0.0
+p2_RealSense_1280 = 0.0
+
+INTRINSIC_CAMERA_REALSENSE_1280 = np.array([[fx_RealSense_1280, 0, cx_RealSense_1280], [0, fy_RealSense_1280, cy_RealSense_1280], [0, 0, 1]])
+DISTORTION_CAMERA_REALSENSE_1280 = np.array([k1_RealSense_1280, k2_RealSense_1280, p1_RealSense_1280, p2_RealSense_1280, k3_RealSense_1280])
 
 class detection:
     def __init__(self):
         rospy.init_node('detection')
-        self.pub = rospy.Publisher('/camera/aruco', Image, queue_size=1)
-        #self.sub = rospy.Subscriber('/camera/color/image_raw', Image, self.image_callback, queue_size=1)
-        # Take compressed image and call the 'image_callback' function with the functioning image
-        self.sub = rospy.Subscriber('/head_front_camera/color/image_raw/compressed', CompressedImage, self.image_compressed_wrapper, queue_size=1)
+        self.pub_torso = rospy.Publisher('/torso_front_camera/color/aruco', Image, queue_size=2)
+        self.pub_front = rospy.Publisher('/head_front_camera/color/aruco', Image, queue_size=2)
+        self.sub_torso = rospy.Subscriber('/torso_front_camera/color/image_raw', Image, self.image_callback, queue_size=2)
+        self.sub_front = rospy.Subscriber('/head_front_camera/color/image_raw/compressed', CompressedImage, self.image_compressed_callback, queue_size=2)
         self.bridge = CvBridge()
         self.received_image = False
-        self.bridge = CvBridge()
+        #self.bridge = CvBridge()
 
-    def image_compressed_wrapper(self, msg):
-        image_wrp = np.asarray(bytearray(msg.data), dtype="uint8")
-        image_wrp = cv2.imdecode(image_wrp, cv2.IMREAD_COLOR) 
-        self.image_callback(image_wrp)
+    def image_callback(self, msg):
+        bridge = CvBridge()
+        self.image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+
+        image = copy.deepcopy(self.image)
+        frame, tvec, rvec = self.get_pose(image, 1280)
+        cv2.imshow('image_torso', frame)
+        cv2.waitKey(1)
+
+        self.pub_torso.publish(bridge.cv2_to_imgmsg(frame, "bgr8"))
 
         
 
-    def image_callback(self, msg):
-        print('immagine arrivata')
+    def image_compressed_callback(self, msg):
+        image_wrp = np.asarray(bytearray(msg.data), dtype="uint8")
+        image_wrp = cv2.imdecode(image_wrp, cv2.IMREAD_COLOR) 
+        #self.image_callback(image_wrp)
+
         bridge = CvBridge()
-        # self.image = bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
-        # image = copy.deepcopy(self.image)
-        frame, tvec, rvec = self.get_pose(msg)
-
-        # Publish the image with the aruco detected
-        self.pub.publish(bridge.cv2_to_imgmsg(frame, "bgr8"))
-
-    def get_pose(self, frame):
-        print('ao')
-        cv2.imshow('image', frame)
+        
+        frame, tvec, rvec = self.get_pose(image_wrp)
+        
+        cv2.imshow('image_front', image_wrp)
         cv2.waitKey(1)
+
+        self.pub_front.publish(bridge.cv2_to_imgmsg(frame, "bgr8"))
+
+    def get_pose(self, frame, width=848):
+        #cv2.imshow('image', frame)
+        #cv2.waitKey(1)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco.Dictionary_get(aruco.DICT_4X4_50))
+        #cv2.imshow('image', gray)  
+        #cv2.waitKey(1)
         #corners, ids, rejectedImgPoints = aruco.detectMarkers(frame, aruco.Dictionary_get(aruco.DICT_4X4_50))
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco.Dictionary_get(aruco.DICT_4X4_50))
 
         if len(corners) > 0:
             ids = ids.flatten()
 
-            rvec, tvec = aruco.estimatePoseSingleMarkers(corners, 0.05, INTRINSIC_CAMERA, DISTORTION_CAMERA)
+            if width == 848:
+                INTRINSIC_USED = INTRINSIC_CAMERA_REALSENSE
+                DISTORTION_USED = DISTORTION_CAMERA_REALSENSE
+            else:
+                INTRINSIC_USED = INTRINSIC_CAMERA_REALSENSE_1280
+                DISTORTION_USED = DISTORTION_CAMERA_REALSENSE_1280
+
+            rvec, tvec = aruco.estimatePoseSingleMarkers(corners, 0.05, INTRINSIC_USED, DISTORTION_USED)
             frame = cv2.aruco.drawDetectedMarkers(frame, corners,ids)
             print('[INFO] rvec: ', rvec)
             print('[INFO] tvec: ', tvec)
             for i in range(len(ids)):
-                frame = cv2.aruco.drawAxis(frame, INTRINSIC_CAMERA, DISTORTION_CAMERA, rvec[i], tvec[i], 0.1)
+                frame = cv2.aruco.drawAxis(frame, INTRINSIC_USED, DISTORTION_USED, rvec[i], tvec[i], 0.1)
             #frame = cv2.aruco.drawAxis(frame, INTRINSIC_CAMERA, DISTORTION_CAMERA, rvec, tvec, 0.1)
             return frame, tvec, rvec
         return frame, None, None
