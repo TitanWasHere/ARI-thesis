c57bd83dc2f62f3a50ca975e3e0a38a523bbf55b
TitanWasHere <andrea.gravili8@gmail.com>
2024-03-27 18:30:12 +0100

    trying to publish

--- aruco_ws/src/detection/scripts/matrix_base.py
+++ aruco_ws/src/detection/scripts/matrix_base.py
@@ -13,18 +13,9 @@ def main():
     tfBuffer = tf2_ros.Buffer(rospy.Duration(10))
     listener = tf2_ros.TransformListener(tfBuffer)
     
-    trasformation = tfBuffer.lookup_transform("odom", "torso_front_camera_color_frame", rospy.Time(), rospy.Duration(1.0))
+    trasformation = tfBuffer.lookup_transform("base_footprint", "torso_front_camera_color_frame", rospy.Time(), rospy.Duration(1.0))
     #trasformation = tfBuffer.lookup_transform("base_footprint", "base_link", rospy.Time(), rospy.Duration(1.0))
     print(trasformation) 
-
-    pos_x = trasformation.transform.translation.x
-    pos_y = trasformation.transform.translation.y
-    pos_z = trasformation.transform.translation.z
-
-    quat_x = trasformation.transform.rotation.x
-    quat_y = trasformation.transform.rotation.y
-    quat_z = trasformation.transform.rotation.z
-    quat_w = trasformation.transform.rotation.w
             
     
     
